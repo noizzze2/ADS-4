72216fc133a23a4fec424bc0466bfa9ae8aab0c0
noizzze2 <noizzze20@gmail.com>
2026-04-05 22:24:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,24 +2,6 @@
 
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len; i++) {
-        if (i > 0 && arr[i] == arr[i - 1]) {
-            continue;
-        }
-        for (int j = i + 1; j < len; j++) {
-            if (arr[i] + arr[j] == value) {
-                count++;
-                while (j + 1 < len && arr[j + 1] == arr[j]) {
-                    j++;
-                }
-            }
-        }
-    }
-    return count;
-}
-
-int countPairs2(int *arr, int len, int value) {
-    int count = 0;
     int left = 0;
     int right = len - 1;
     while (left < right) {
@@ -38,6 +20,24 @@ int countPairs2(int *arr, int len, int value) {
             left++;
         } else {
             right--;
+        }
+    }
+    return count;
+}
+
+int countPairs2(int *arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        if (i > 0 && arr[i] == arr[i - 1]) {
+            continue;
+        }
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+                while (j + 1 < len && arr[j + 1] == arr[j]) {
+                    j++;
+                }
+            }
         }
     }
     return count;
