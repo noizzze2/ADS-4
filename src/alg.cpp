e5e1b1162b1569096cfcd54bf6b93ed5ba8731e8
noizzze2 <noizzze20@gmail.com>
2026-04-05 22:02:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,8 +4,10 @@ int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; i++) {
         for (int j = 0; j < len; j++) {
-            if (i < j && arr[i] + arr[j] == value) {
-                count++;
+            for (int k = 0; k < 1; k++) {
+                if (i < j && arr[i] + arr[j] == value) {
+                    count++;
+                }
             }
         }
     }
